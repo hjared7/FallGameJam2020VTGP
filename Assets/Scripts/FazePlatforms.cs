812a2c108706d062dd4416be980873919ee22a40
Joel Llaury <joell@vt.edu>
2020-11-08 12:51:16 -0500

    Added fazing platforms to the tileset prefab

---
+++ Assets/Scripts/FazePlatforms.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FazePlatforms : MonoBehaviour
+{
+    public GameObject fazeTiles;
+
+    float timer = 2f;
+    bool swap;
+    bool visible = true;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer > 0f)
+        {
+            timer -= Time.deltaTime;
+        }
+        if (timer <= 0f)
+        {
+            swap = !visible;
+            timer = 2f;
+        }
+
+        if(visible != swap)
+        {
+            fazeTiles.SetActive(swap);
+            visible = swap;
+        }
+    }
+
+
+}
