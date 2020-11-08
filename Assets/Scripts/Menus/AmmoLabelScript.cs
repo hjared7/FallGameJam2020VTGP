c3e41e34a04bcb2c0532e5691d5e599fdfe2f04f
Jadennl <Jadennl@vt.edu>
2020-11-07 22:32:30 -0500

    Adding UI and making ammo easier to access from other scripts.

---
+++ Assets/Scripts/Menus/AmmoLabelScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AmmoLabelScript : MonoBehaviour
+{
+    private Text labelText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        labelText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        labelText.text = "X " + GameControl.control.ammo;
+    }
+}
