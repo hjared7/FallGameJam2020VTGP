f0997cf2161375e3b5a3add796177974941599d7
Ben Schmidt <54752448+YellowArchitect@users.noreply.github.com>
2020-11-08 16:50:58 -0500

    Win Gallon

---
+++ Assets/Scripts/Interactables/GallonBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GallonBehaviour : MonoBehaviour
+{
+    public float frequency = 0.5f;
+    public float amplitude = 1f;
+
+    private Vector3 tempPos;
+    private Vector3 originalPos;
+
+    void Awake()
+    {
+        originalPos = transform.position;
+    }
+    void Update()
+    {
+        // Float up/down with a Sin()
+        tempPos = originalPos;
+        tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+
+        transform.position = tempPos;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            SceneManager.LoadScene(3);
+        }
+    }
+}
