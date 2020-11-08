d8844bcffc15bfe40adad4411bd7d636f317ce7e
hjared7 <hjared7@vt.edu>
2020-11-08 09:28:23 -0800

    Fixed the BONE

--- Assets/Scripts/Player/BoneBehaviour.cs
+++ Assets/Scripts/Player/BoneBehaviour.cs
@@ -6,22 +6,15 @@ using UnityEngine.PlayerLoop;
 
 public class BoneBehaviour : MonoBehaviour
 {
-
-    //Time before bone starts falling.
-    public float falloffTimer = 1f;
-    //Amount the bone falls by.
-    public float falloffGravity = 1f;
-    //Time befor bone despawns.
-    public float despawnTimer = 3f;
+    public float despawnTimer;
     //Speed of bone when thrown.
-    public float throwVelocity = 5f;
+    public float throwVelocity;
     //Direction to throw the bone.
     public float throwDirection;
 
     Rigidbody2D rb;
     BoxCollider2D bc;
     private float timer;
-
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -35,24 +28,27 @@ public class BoneBehaviour : MonoBehaviour
     }
 
     // Basic collision logic.
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.layer == 8)
         {
+            Destroy(this.gameObject);
+        }
+        
+        else if(collision.gameObject.layer == 9)
+        {
+            //Remove enemy health or destroy them
+            Destroy(collision.gameObject);
             Destroy(this.gameObject);
         }
     }
 
     void FixedUpdate()
     {
-        rb.velocity = new Vector2(throwVelocity * throwDirection, rb.velocity.y);
+        rb.velocity = new Vector2(throwVelocity * throwDirection, 0);
         if (timer >= despawnTimer)
         {
             Destroy(this.gameObject);
-        }
-        if(timer >= falloffTimer)
-        {
-            rb.gravityScale = falloffGravity;
         }
     }
 }
